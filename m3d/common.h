05209a7d641653f28cfdbec8378125180dfce234
Andy Brown <adb1413@rit.edu>
2012-09-09 13:45:49 -0400

    Added initial configure script and Makefile

---
+++ m3d/common.h
@@ -0,0 +1,15 @@
+/*
+ * common.h
+ *
+ * Author
+ *     Andrew Brown <adb1413@rit.edu>
+ */
+#ifndef M3D_COMMON_H
+#define M3D_COMMON_H
+#include <cstdlib>
+
+#ifndef SIZE_MAX
+#define SIZE_MAX ((size_t) -1)
+#endif
+
+#endif
